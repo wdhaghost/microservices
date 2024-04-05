6f832eeb8022caf0cdd4583b04e070396fe2f546
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2024-04-05 14:19:48 +0200

    Ajout des routes pour les étudiants, les groupes et les cours

--- SchoolRepo/routes/api.php
+++ SchoolRepo/routes/api.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\CourseGroupController;
+use App\Http\Controllers\GroupController;
+use App\Http\Controllers\StudentController;
+use App\Models\Course;
+use App\Models\Group;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use PHPUnit\Framework\MockObject\Builder\Stub;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +23,27 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+
+Route::controller(StudentController::class)->group(function () {
+    Route::get('/students','index');
+    Route::get('/students/{id}','show');
+    Route::get('/students/{id}/group','getGroup');
+});
+
+Route::prefix('groups')->group  (function () {
+    Route::get('/',[GroupController::class ,'index']);
+    Route::get('/{id}', [GroupController::class ,'show']);
+    Route::get('/{id}/students', [GroupController::class ,'getStudents']);
+    Route::get('/{id}/courses', [GroupController::class ,'getCourses']);
+
+});
+
+Route::prefix('courses')->group(function () {
+    Route::get('/', [CourseController::class, 'index']);
+    Route::get('/{id}', [CourseController::class, 'show']);
+    Route::get('/{id}/groups', [CourseController::class, 'getGroups']);
+    Route::get('/{id}/students', [CourseController::class, 'getStudents']);
 });
