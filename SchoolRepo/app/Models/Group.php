9d6d08ead83cc2b194d421fdfaf88f27d874ca66
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2024-04-05 11:17:56 +0200

    Ajout de relations de groupe avec les cours et les étudiants

--- SchoolRepo/app/Models/Group.php
+++ SchoolRepo/app/Models/Group.php
@@ -7,5 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Group extends Model
 {
+    public function courses(){
+        return $this->belongsToMany(Course::class);
+    }
+    public function students(){
+        return $this->hasMany(Student::class);
+    }
     use HasFactory;
 }
